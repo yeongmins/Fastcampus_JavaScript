73f4c7de4e09598280705311666d7589906238fc
yeongmins <oxox123456@naver.com>
2023-08-16 14:34:30 +0900

    Feat: 임직원 삭제 기능 추가

--- assets/javascript/delete.js
+++ assets/javascript/delete.js
@@ -1,37 +1,39 @@
+// DOMContentLoaded 이벤트가 발생할 때 실행되는 함수
 document.addEventListener("DOMContentLoaded", function () {
-  // delete_button 요소를 가져옵니다.
+  // 삭제 버튼 요소 가져오기
   const deleteButton = document.getElementById("delete_button");
 
-  // delete_button을 클릭했을 때의 동작을 정의합니다.
+  // 삭제 버튼에 클릭 이벤트 리스너 추가
   deleteButton.addEventListener("click", function () {
-    // 다른 요소의 input checkbox 상태를 가져옵니다.
-    const checkbox = document.querySelector("input[type='checkbox']");
+    // 체크된 체크박스 요소들 가져오기
+    const checkboxes = document.querySelectorAll("input[type='checkbox']:checked");
 
-    // input checkbox가 체크되어 있는지 확인합니다.
-    if (checkbox.checked) {
-      // SweetAlert2 알림을 생성합니다.
+    if (checkboxes.length > 0) {
       Swal.fire({
         title: "임직원 삭제 확인",
-        text: "정말로 임직원을 삭제하시겠습니까?",
+        text: "선택한 임직원을 삭제하시겠습니까?",
         icon: "warning",
         showCancelButton: true,
         confirmButtonText: "삭제",
         cancelButtonText: "취소",
       }).then((result) => {
-        // SweetAlert2에서 선택된 결과를 확인합니다.
         if (result.isConfirmed) {
-          // 삭제 동작을 수행하는 함수를 호출합니다.
-          // 이곳에 실제 삭제 로직을 구현하세요.
-          // 예를 들면, 서버로 요청을 보내서 데이터를 삭제하는 등의 작업이 가능합니다.
-          // 이 예제에서는 아무 작업도 수행하지 않습니다.
-          Swal.fire("삭제됨!", "임직원이 삭제되었습니다.", "success");
+          // 선택된 체크박스의 부모 요소인 리스트 아이템을 삭제
+          checkboxes.forEach((checkbox) => {
+            const listItem = checkbox.closest(".list_item");
+            if (listItem) {
+              listItem.remove();
+            }
+          });
+
+          Swal.fire("삭제됨!", "선택한 임직원이 삭제되었습니다.", "success");
         }
       });
     } else {
       Swal.fire(
         "체크박스 미선택",
         "삭제하려는 임직원을 선택하세요.",
-        "warning",
+        "warning"
       );
     }
   });
--- assets/javascript/modal.js
+++ assets/javascript/modal.js
@@ -1,69 +1,74 @@
+// 모달을 열기 위한 요소들을 변수에 저장
+const registerButton = document.getElementById("register_button"); // 등록 버튼
+const closeButton = document.getElementById("close_button"); // 닫기 버튼
+const registerModal = document.getElementById("register"); // 등록 모달 창
+const body = document.body; // body 요소
+
+// 등록 버튼에 클릭 이벤트 리스너 추가
+registerButton.addEventListener("click", () => {
+  registerModal.classList.add("show"); // 등록 모달 창에 "show" 클래스 추가하여 표시
+  body.classList.add("modal-open"); // body에 "modal-open" 클래스 추가하여 스크롤 방지
+});
+
+// 닫기 버튼에 클릭 이벤트 리스너 추가
+closeButton.addEventListener("click", () => {
+  registerModal.classList.remove("show"); // 등록 모달 창에서 "show" 클래스 제거하여 숨김
+  body.classList.remove("modal-open"); // body에서 "modal-open" 클래스 제거하여 스크롤 가능하게
+});
+
+// 등록 모달 내에서 실제 등록 작업을 처리하는 부분
 document.getElementById("modal_register").addEventListener("click", function () {
-  const name = document.getElementById("name_ID").value;
-  const email = document.getElementById("email_ID").value;
-  const phone = document.getElementById("phone_ID").value;
-  const rank = document.getElementById("rank_ID").value;
-  const imgInput = document.getElementById("file_input");
-  const imgURL = imgInput.files[0] ? URL.createObjectURL(imgInput.files[0]) : "";
+  // 입력된 정보 가져오기
+  const name = document.getElementById("name_ID").value; // 이름
+  const email = document.getElementById("email_ID").value; // 이메일
+  const phone = document.getElementById("phone_ID").value; // 전화번호
+  const rank = document.getElementById("rank_ID").value; // 직급
+  const imgInput = document.getElementById("file_input"); // 파일 업로드 입력 요소
+  const imgURL = imgInput.files[0] ? URL.createObjectURL(imgInput.files[0]) : ""; // 이미지 URL 생성
 
-  document.getElementById("name_text").innerHTML = name;
-  document.getElementById("email_text").innerHTML = email;
-  document.getElementById("phone_text").innerHTML = phone;
-  document.getElementById("rank_text").innerHTML = rank;
-  document.getElementById("view_img_ID").src = imgURL;
+  // 등록된 정보를 모달 내에 표시하는 부분
+  document.getElementById("name_text").innerHTML = name; // 이름 표시
+  document.getElementById("email_text").innerHTML = email; // 이메일 표시
+  document.getElementById("phone_text").innerHTML = phone; // 전화번호 표시
+  document.getElementById("rank_text").innerHTML = rank; // 직급 표시
+  document.getElementById("view_img_ID").src = imgURL; // 이미지 표시
 
-  const mainList = document.getElementById("main_list");
-  const listItem = document.createElement("div");
-  listItem.classList.add("list_item");
+  // 새로운 리스트 아이템 생성 및 추가
+  const mainList = document.getElementById("main_list"); // 리스트 컨테이너
+  const listItem = document.createElement("div"); // 새로운 리스트 아이템 요소 생성
+  listItem.classList.add("list_item"); // 생성된 아이템에 "list_item" 클래스 추가
   
-  const listInput = document.createElement("input");
-  listInput.classList.add("list_input");
-  listInput.type = "checkbox";
-  listInput.name = "list_checkbox";
-  listItem.appendChild(listInput);
+  // 리스트 아이템 내부 요소들 생성
+  const listInput = document.createElement("input"); // 체크박스 생성
+  listInput.classList.add("list_input"); // 생성된 체크박스에 "list_input" 클래스 추가
+  listInput.type = "checkbox"; // 입력 타입을 체크박스로 설정
+  listInput.name = "list_checkbox"; // 체크박스의 이름 설정
+  listItem.appendChild(listInput); // 체크박스를 리스트 아이템에 추가
   
-  const itemWrap = document.createElement("div"); // Create the item_wrap div
-  itemWrap.classList.add("item_wrap"); // Add the class
+  const itemWrap = document.createElement("div"); // 아이템 내용을 감싸는 div 생성
+  itemWrap.classList.add("item_wrap"); // 생성된 div에 "item_wrap" 클래스 추가
   
-  const itemImg = document.createElement("div"); // Create the item_img div
-  itemImg.classList.add("item_img"); // Add the class
+  const itemImg = document.createElement("div"); // 이미지를 감싸는 div 생성
+  itemImg.classList.add("item_img"); // 생성된 div에 "item_img" 클래스 추가
   
-  const profileImage = document.createElement("img");
-  profileImage.src = imgURL;
-  profileImage.alt = "프로필 이미지";
-  itemImg.appendChild(profileImage); // Append the image to item_img
+  const profileImage = document.createElement("img"); // 이미지 요소 생성
+  profileImage.src = imgURL; // 이미지의 소스 설정
+  profileImage.alt = "프로필 이미지"; // 이미지의 대체 텍스트 설정
+  itemImg.appendChild(profileImage); // 이미지를 "item_img" div에 추가
   
-  itemWrap.appendChild(itemImg); // Append item_img to item_wrap
+  itemWrap.appendChild(itemImg); // "item_img" div를 "item_wrap" div에 추가
   
   const spans = [name, email, phone, rank];
   for (const spanText of spans) {
-      const span = document.createElement("span");
-      span.textContent = spanText;
-      itemWrap.appendChild(span); // Append the span to item_wrap
+      const span = document.createElement("span"); // 각 정보를 담는 span 생성
+      span.textContent = spanText; // span의 텍스트 내용 설정
+      itemWrap.appendChild(span); // span을 "item_wrap" div에 추가
   }
   
-  listItem.appendChild(itemWrap); // Append item_wrap to listItem
-  mainList.appendChild(listItem);
+  listItem.appendChild(itemWrap); // "item_wrap" div를 리스트 아이템에 추가
+  mainList.appendChild(listItem); // 리스트 아이템을 리스트 컨테이너에 추가
   
-  // 모달 닫기 코드
-  const registerModal = document.getElementById("register");
-  const body = document.body;
-  registerModal.classList.remove("show");
-  body.classList.remove("modal-open");
-});
-
-// 모달 열기 코드
-const registerButton = document.getElementById("register_button");
-const closeButton = document.getElementById("close_button");
-const registerModal = document.getElementById("register");
-const body = document.body;
-
-registerButton.addEventListener("click", () => {
-  registerModal.classList.add("show");
-  body.classList.add("modal-open");
-});
-
-closeButton.addEventListener("click", () => {
-  registerModal.classList.remove("show");
-  body.classList.remove("modal-open");
+  // 등록이 완료되었으므로 모달을 닫음
+  registerModal.classList.remove("show"); // 모달 창에서 "show" 클래스 제거하여 숨김
+  body.classList.remove("modal-open"); // body에서 "modal-open" 클래스 제거하여 스크롤 가능하게
 });
