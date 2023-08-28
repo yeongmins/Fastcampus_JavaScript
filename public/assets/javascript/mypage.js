fe996afd5ee884f6a9d5c7efdc16a0584cdfccca
yeongmins <oxox123456@naver.com>
2023-08-29 04:59:38 +0900

    Refactor: 코드 개선

--- public/assets/javascript/mypage.js
+++ public/assets/javascript/mypage.js
@@ -21,36 +21,35 @@ mypageFileInput.addEventListener("change", () => {
   }
 });
 
-
 // "mypage" 버튼 클릭 시 이벤트 리스너
 mypageButton.addEventListener("click", () => {
   // 각각의 필요한 요소들을 가져옴
-  const nameSpan = document.getElementById("name_span");
-  const emailSpan = document.getElementById("email_span");
-  const phoneSpan = document.getElementById("phone_span");
-  const rankSpan = document.getElementById("rank_span");
+  const myNameSpan = document.getElementById("name_span");
+  const myEmailSpan = document.getElementById("email_span");
+  const myPhoneSpan = document.getElementById("phone_span");
+  const myRankSpan = document.getElementById("rank_span");
 
-  const nameInput = document.getElementById("mypage_name_ID");
-  const emailInput = document.getElementById("mypage_email_ID");
-  const phoneInput = document.getElementById("mypage_phone_ID");
-  const rankInput = document.getElementById("mypage_rank_ID");
+  const myNameInput = document.getElementById("mypage_name_ID");
+  const myEmailInput = document.getElementById("mypage_email_ID");
+  const myPhoneInput = document.getElementById("mypage_phone_ID");
+  const myRankInput = document.getElementById("mypage_rank_ID");
 
   // 입력된 값을 화면에 보여줌
-  nameInput.value = nameSpan.textContent;
-  emailInput.value = emailSpan.textContent;
-  phoneInput.value = phoneSpan.textContent;
-  rankInput.value = rankSpan.textContent;
+  myNameInput.value = myNameSpan.textContent;
+  myEmailInput.value = myEmailSpan.textContent;
+  myPhoneInput.value = myPhoneSpan.textContent;
+  myRankInput.value = myRankSpan.textContent;
 
   // 텍스트로도 입력된 값을 화면에 보여줌
-  const nameText = document.getElementById("mypage_name_text");
-  const emailText = document.getElementById("mypage_email_text");
-  const phoneText = document.getElementById("mypage_phone_text");
-  const rankText = document.getElementById("mypage_rank_text");
+  const myNameText = document.getElementById("mypage_name_text");
+  const myEmailText = document.getElementById("mypage_email_text");
+  const myPhoneText = document.getElementById("mypage_phone_text");
+  const myRankText = document.getElementById("mypage_rank_text");
 
-  nameText.textContent = nameSpan.textContent;
-  emailText.textContent = emailSpan.textContent;
-  phoneText.textContent = phoneSpan.textContent;
-  rankText.textContent = rankSpan.textContent;
+  myNameText.textContent = myNameSpan.textContent;
+  myEmailText.textContent = myEmailSpan.textContent;
+  myPhoneText.textContent = myPhoneSpan.textContent;
+  myRankText.textContent = myRankSpan.textContent;
 
   // 기존 이미지의 소스를 가져와서 프로필 이미지 요소에 설정
   const myImgSrc = myImg.getAttribute("src");
@@ -67,15 +66,15 @@ mypageButton.addEventListener("click", () => {
     }
 
     // 입력된 값들을 각각의 요소에 반영
-    nameSpan.textContent = nameInput.value;
-    emailSpan.textContent = emailInput.value;
-    phoneSpan.textContent = phoneInput.value;
-    rankSpan.textContent = rankInput.value;
+    myNameSpan.textContent = myNameInput.value;
+    myEmailSpan.textContent = myEmailInput.value;
+    myPhoneSpan.textContent = myPhoneInput.value;
+    myRankSpan.textContent = myRankInput.value;
 
-    nameText.textContent = nameInput.value;
-    emailText.textContent = emailInput.value;
-    phoneText.textContent = phoneInput.value;
-    rankText.textContent = rankInput.value;
+    myNameText.textContent = myNameInput.value;
+    myEmailText.textContent = myEmailInput.value;
+    myPhoneText.textContent = myPhoneInput.value;
+    myRankText.textContent = myRankInput.value;
 
     // 모달 닫기
     mypageModal.classList.remove("show");
@@ -92,3 +91,4 @@ mypageCloseButton.addEventListener("click", () => {
   mypageModal.classList.remove("show");
   mypageBody.classList.remove("modal-open");
 });
+
--- public/assets/javascript/search.js
+++ public/assets/javascript/search.js
@@ -4,27 +4,18 @@ const mainList = document.getElementById("main_list");
 
 // 검색 버튼 클릭 시 검색 기능 수행
 searchButton.addEventListener("click", () => {
-  const searchTerm = searchInput.value.toLowerCase(); // 입력된 검색어를 소문자로 변환
+  const searchTerm = searchInput.value.toLowerCase(); 
 
-  const listItems = mainList.querySelectorAll(".list_item"); // 모든 리스트 아이템 가져오기
+  const listItems = mainList.querySelectorAll(".list_item"); 
 
   // 각 리스트 아이템에 대해 검색어와 비교하여 필터링
   listItems.forEach((item) => {
-    const spans = item.querySelectorAll(".item_wrap span"); // 리스트 아이템 내의 모든 span 가져오기
-    let shouldShow = false; // 아이템을 표시할지 여부를 결정하는 플래그
-
-    spans.forEach((span) => {
-      const text = span.textContent.toLowerCase();
-      if (text.includes(searchTerm)) {
-        shouldShow = true; // 검색어가 포함된 span이 있으면 표시할 플래그를 true로 설정
-      }
+    const spans = item.querySelectorAll(".item_wrap span");
+    const shouldShow = Array.from(spans).some((span) => {
+      return span.textContent.toLowerCase().includes(searchTerm);
     });
 
     // 아이템을 표시하거나 숨김 처리
-    if (shouldShow) {
-      item.style.display = "flex"; // 표시
-    } else {
-      item.style.display = "none"; // 숨김
-    }
+    item.style.display = shouldShow ? "flex" : "none";
   });
 });
